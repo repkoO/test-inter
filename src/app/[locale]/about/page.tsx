6e3a5b97a203a7df936107f2691b815a23ecc4cb
Kirill Medvedev <repkomail@gmail.com>
2025-09-24 22:54:33 +0200

    feat: add few pages

---
+++ src/app/[locale]/about/page.tsx
@@ -0,0 +1,19 @@
+import { useTranslations } from "next-intl";
+import styles from "./page.module.css";
+import Link from "next/link";
+import LanguageSwitcher from "@/components/LanguageSwitcher";
+
+function About() {
+  const t = useTranslations("About");
+  return (
+    <div className={styles.container}>
+      <LanguageSwitcher />
+      <p className={styles.text}>{t("text")}</p>
+      <div className={styles.link}>
+        <Link href={"/"}>{t("link")}</Link>
+      </div>
+    </div>
+  );
+}
+
+export default About;
--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -2,16 +2,23 @@ import Image from "next/image";
 import styles from "./page.module.css";
 import LanguageSwitcher from "@/components/LanguageSwitcher";
 import { useTranslations } from "next-intl";
+import Link from "next/link";
 
 export default function Home() {
   const t = useTranslations("Homepage");
 
   return (
-    <>
+    <main className={styles.main}>
       <LanguageSwitcher />
       <div className={styles.page}>
         <h1>{t("title")}</h1>
       </div>
-    </>
+      <div className={styles.description}>
+        <p className={styles.text}>{t("description")}</p>
+      </div>
+      <div className={styles.link}>
+        <Link href={"/about"}>{t("link")}</Link>
+      </div>
+    </main>
   );
 }
